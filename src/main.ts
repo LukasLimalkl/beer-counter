b31a833f70b392c51e1c2921493adb3b34e08399
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-17 15:17:40 -0300

    feat: confing html and style

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,17 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-  <h1> Hello World </h1>
+    <h1> Ranking Semana 1 </h1>
+    <div class="container">
+        <div class="coluna">
+            <h2>Nome</h2>
+            <p></p>
+        </div>
+        <div class="coluna">
+            <h2>Cervejas Tomadas</h2>
+            <p></p>
+        </div>
+    </div>
   </div>
 `
 
