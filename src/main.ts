0bbc7a7b5eb421a06cbebb674ca382ac6d83b3d9
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-17 15:55:30 -0300

    feat: adding styles

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <h1> Ranking Semana 1 </h1>
+  <div class="main-container">
+    <h1 class="title" > Ranking Semana 1 </h1>
     <div class="container">
-        <div class="coluna">
+        <div class="coluna name">
             <h2>Nome</h2>
             <p></p>
         </div>
-        <div class="coluna">
+        <div class="coluna beer">
             <h2>Cervejas Tomadas</h2>
             <p></p>
         </div>
