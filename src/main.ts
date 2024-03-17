e7a6cad50bc33241f6ec745bd358e4db9dfd38e1
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-17 16:20:47 -0300

    feat: testing styles

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,31 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main-container">
-    <h1 class="title" > Ranking Semana 1 </h1>
+    <h1 class="title">Ranking Semana 1</h1>
     <div class="container">
         <div class="coluna name">
-            <h2>Nome</h2>
-            <p></p>
+            <div class="box-title">
+              <h2>Nome</h2>
+            </div>
+            <div class="list-name">
+                <ul> 
+                    <li>Nome 1</li>
+                    <li>Nome 2</li>
+                    <li>Nome 3</li>
+                </ul>
+            </div>
         </div>
         <div class="coluna beer">
-            <h2>Cervejas Tomadas</h2>
-            <p></p>
+            <div class="box-title">
+              <h2>Cervejas Tomadas</h2>
+            </div>
+            <div class="list-beer">
+                <ul> 
+                    <li>3</li>
+                    <li>5</li>
+                    <li>2</li>
+                </ul>
+            </div>
         </div>
     </div>
   </div>
