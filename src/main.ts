94dca6a64050e824b25df8d3b368d7c51ba590ec
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-21 20:39:47 -0300

    feat: fixing the html

--- src/main.ts
+++ src/main.ts
@@ -4,31 +4,28 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main-container">
     <h1 class="title">Ranking Semana 1</h1>
     <div class="container">
-        <div class="coluna name">
+        <div class="coluna">
             <div class="box-title">
                 <h2>Name</h2>
+                <h2>Cervejas Tomadas </h2>
             </div>
             <div class="barra"></div>
-            <div class="list-name">
+            <div class="list-container">
                 <ul> 
-                    <li>Nome 1</li>
+                    <div class="list-box"> 
+                        <li>Nome 1</li>
+                        <li> 1</li>
+                    </div>
                     <div class="barra"></div>
-                    <li>Nome 2</li>
+                    <div class="list-box"> 
+                        <li>Nome 1</li>
+                        <li> 1</li>
+                    </div>
                     <div class="barra"></div>
-                    <li>Nome 3</li>
-                </ul>
-            </div>
-        </div>
-        <div class="coluna beer">
-            <div class="box-title">
-                <h2>Cervejas Tomadas</h2>
-            </div>
-            <div class="barra"></div>
-            <div class="list-beer">
-                <ul> 
-                    <li>3</li>
-                    <li>5</li>
-                    <li>2</li>
+                    <div class="list-box"> 
+                        <li>Nome 1</li>
+                        <li> 1</li>
+                    </div>
                 </ul>
             </div>
         </div>
