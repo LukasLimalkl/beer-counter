d04c48c830a71131b38f475a1080af5c88ee9e4b
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-22 20:22:29 -0300

    feat: adding template of list

---
+++ src/template.ts
@@ -0,0 +1,11 @@
+const template = `
+    <div class="list-box"> 
+        <li>Nome 1</li>
+        <li> 1</li>
+    </div>
+    <div class="button-box">
+        <button class="button"> - </button>
+        <button class="button"> + </button>
+    </div>
+    <div class="barra"></div>
+`
