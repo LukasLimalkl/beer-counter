06b73e6f3147c3375e61e36e33a6c6b296740eb6
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-23 15:40:37 -0300

    feat: adding counter

--- src/listTemplate.ts
+++ src/listTemplate.ts
@@ -1,7 +1,7 @@
 export const listTemplate = `
         <div class="list-box"> 
             <li>Nome 1</li>
-            <li> 1</li>
+            <li> </li>
             <div class="button-box">
                 <button class="button" onclick="myFunction()"> - </button>
                 <button class="button" onclick="myFunction()"> + </button>
