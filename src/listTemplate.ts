4ccad444bfdb26f113d30d93b6277b3e5b026310
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-22 21:11:20 -0300

    feat: fixing listTemplate

--- src/listTemplate.ts
+++ src/listTemplate.ts
@@ -1,15 +1,11 @@
-export function listTemplate() {
-    const listTemplate = `
-    <div class="list-box"> 
-        <li>Nome 1</li>
-        <li> 1</li>
-    </div>
-    <div class="button-box">
-        <button class="button"> - </button>
-        <button class="button"> + </button>
-    </div>
-    <div class="barra"></div>
+export const listTemplate = `
+        <div class="list-box"> 
+            <li>Nome 1</li>
+            <li> 1</li>
+            <div class="button-box">
+                <button class="button"> - </button>
+                <button class="button"> + </button>
+            </div>
+        </div>
+        
 `
-    return listTemplate
-
-}
