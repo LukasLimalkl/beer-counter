11195fbcac30459b3e4281c8c3368131bd9d640a
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-24 18:58:40 -0300

    feat: adding counter

---
+++ src/counter.ts
@@ -0,0 +1,13 @@
+let counter: number = 0;
+
+export function getCounter(): number {
+    return counter;
+}
+
+export function incrementCounter(): void {
+    counter++;
+}
+
+export function decrementCounter(): void {
+    counter--;
+}
