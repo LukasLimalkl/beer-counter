dd90ac98c81a8fa419718744aeb9815d6e2cd5a2
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-24 18:59:06 -0300

    feat: adding less counter fn

---
+++ src/lessCounter.ts
@@ -0,0 +1,10 @@
+import { decrementCounter, getCounter } from "./counter";
+
+export function lessCounter(): void {
+    decrementCounter();
+    const listCounter: HTMLElement | null = document.querySelector("#counterElement");
+
+    if (listCounter) {
+        listCounter.innerHTML = getCounter().toString();
+    }
+}
