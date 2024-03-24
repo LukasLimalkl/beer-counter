afcf267d043dd2dcdf7d029477a73904a6a0937d
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-24 18:46:22 -0300

    feat: fixing error on list

--- src/listTemplate.ts
+++ src/listTemplate.ts
@@ -1,4 +1,4 @@
-import { plusCounter } from "./counter";
+import { plusCounter } from "./plusCounter";
 
 document.addEventListener("DOMContentLoaded", () => {
     const increaseButton: HTMLButtonElement | null = document.querySelector("#increaseButton");
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 export const listTemplate = `
     <div class="list-box"> 
         <li>Nome 1</li>
-        <li id="counterElement"></li>
+        <li id="counterElement">0</li>
         <div class="button-box">
             <button class="button" onclick="myFunction()"> - </button>
             <button id="increaseButton" class="button"> + </button>
