265fadc1561ab0a1aad15ef0e78471ac5d2af415
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-24 12:29:47 -0300

    feat: removing counter

--- src/listTemplate.ts
+++ src/listTemplate.ts
@@ -1,11 +1,11 @@
 export const listTemplate = `
-        <div class="list-box"> 
-            <li>Nome 1</li>
-            <li> </li>
-            <div class="button-box">
-                <button class="button" onclick="myFunction()"> - </button>
-                <button class="button" onclick="myFunction()"> + </button>
-            </div>
+    <div class="list-box"> 
+        <li>Nome 1</li>
+        <li id="counterElement"></li>
+        <div class="button-box">
+            <button class="button" onclick="myFunction()"> - </button>
+            <button id="increaseButton" class="button"> + </button>
         </div>
-        
-`
+    </div>
+`;
+
