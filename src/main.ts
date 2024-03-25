b16f98963df99cb455e8cc481e778c94cb90231a
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-24 21:33:41 -0300

    feat: adding title counter and style

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
-import { listTemplate } from './listTemplate'
-import './style.css'
+import { listTemplate } from './listTemplate';
+import './style.css';
+import { titleCounter } from './titleCounter';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const increaseButton: HTMLButtonElement | null = document.querySelector("#titleCounter");
+
+    if (increaseButton) {
+        increaseButton.addEventListener("click", titleCounter);
+    }
+});
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main-container">
-    <h1 class="title">Ranking Semana 1</h1>
+    <button class="button-title" id="titleCounter"> Outra Semana </button>
+    <div class="main-title"> 
+        <h1 class="title" >Ranking Semana <h1 id="counterTitle">1</h1> </h1>
+    </div>
     <div class="container">
         <div class="coluna">
             <div class="box-title">
@@ -19,4 +31,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </div>
   </div>
 `
+
+
 
