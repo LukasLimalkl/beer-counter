1cbb151872e5a38f0b4c500dc525ccecbdf9e525
LukasLimalkl <lukinhaslima063@gmail.com>
2024-03-25 20:57:18 -0300

    feat: finhisng style of  title button

--- src/main.ts
+++ src/main.ts
@@ -20,9 +20,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="main-container">
-    <div class="main-title-box">
-        <button class="button-title" id="plusTitleCounter"> + </button>
+    <div class="button-box">
         <button class="button-title" id="lessTitleCounter"> - </button>
+        <button class="button-title" id="plusTitleCounter"> + </button>
     </div>
     <div class="main-title"> 
         <h1 class="title" >Ranking Semana <h1 id="counterTitle">1</h1> </h1>
