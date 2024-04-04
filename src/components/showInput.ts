6ab00eecc19656050138e13c709b50c727e14ad1
LukasLimalkl <lukinhaslima063@gmail.com>
2024-04-03 21:28:53 -0300

    feat: starting fn to add names

--- src/components/showInput.ts
+++ src/components/showInput.ts
@@ -1,23 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const addClient: HTMLElement | null = document.getElementById('hide')
+  const addClient: HTMLElement | null = document.getElementById('hide');
 
   function ButtonAction() {
-    if (!addClient) return
+    if (!addClient) return;
 
     if (addClient.style.display == 'flex') {
-      addClient.style.display = 'none'
+      addClient.style.display = 'none';
     } else {
-      addClient.style.display = 'flex'
+      addClient.style.display = 'flex';
     }
   }
 
   const showInputButton: HTMLButtonElement | null =
-    document.querySelector('#showInputButton')
+    document.querySelector('#showInputButton');
 
   if (showInputButton) {
-    showInputButton.addEventListener('click', ButtonAction)
+    showInputButton.addEventListener('click', ButtonAction);
   }
-})
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const items = [];
+
+  const inputValue = document.querySelector('.input-client');
+  items.push(inputValue);
+
+  console.log(items);
+});
 
 export const showInput = `
     <div class="show-box">
@@ -27,4 +36,4 @@ export const showInput = `
              <button class="add-button"> Adicionar nome </button>
         </div>
     </div>
-`
+`;
